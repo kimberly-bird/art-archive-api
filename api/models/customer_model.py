f8bce3f94bd16d569f0d731f8b76f9f6c81127ad
Joe Shepherd <joeshepmedia@gmail.com>
2018-08-28 11:34:52 -0500

    Working auth and Customer

--- api/models/customer_model.py
+++ api/models/customer_model.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
-  user = models.OneToOneField(User, on_delete=models.Cascade)
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
   created = models.DateTimeField(auto_now_add=True)
   street_address = models.CharField(max_length=100)
   city = models.CharField(max_length=50)
