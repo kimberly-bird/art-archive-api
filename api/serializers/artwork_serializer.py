877b9c93c1cd42709c7067b0ae3b4278173553b2
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-09-28 10:22:31 -0500

    resolved issues with foreign key data on artwork table

--- api/serializers/artwork_serializer.py
+++ api/serializers/artwork_serializer.py
@@ -9,10 +9,12 @@ from .owner_serializer import OwnerSerializer
 
 class ArtworkSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.ReadOnlyField(source='user.url')
-    art_type = serializers.ReadOnlyField(source='art_type.url')
-    artist = serializers.ReadOnlyField(source='artist.url')
-    condition = serializers.ReadOnlyField(source='condition.url')
-    owner = serializers.ReadOnlyField(source='owner.url')
+    art_type = serializers.ReadOnlyField(source='art_type.name')
+    artist_first = serializers.ReadOnlyField(source='artist.first_name')
+    artist_last = serializers.ReadOnlyField(source='artist.last_name')
+    condition = serializers.ReadOnlyField(source='condition.name')
+    owner_first = serializers.ReadOnlyField(source='owner.first_name')
+    owner_last = serializers.ReadOnlyField(source='owner.last_name')
 
     class Meta:
         model = Artwork
@@ -21,9 +23,11 @@ class ArtworkSerializer(serializers.HyperlinkedModelSerializer):
             'url',
             'user',
             'art_type',
-            'artist',
+            'artist_first',
+            'artist_last',
             'condition',
-            'owner',
+            'owner_first',
+            'owner_last',
             'title',
             'image_url',
             'year_signed',
